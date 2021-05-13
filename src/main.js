4d3882f1e1b446d68ccd68a0d9fad335a6bc2613
Stacie Taitague <stacie.taitague@gmail.com>
2021-05-12 20:37:41 -0400

    links backend

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import "bootstrap";
 // Vue.use(BootstrapVue);
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://stormy-bayou-04974.herokuapp.com" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
